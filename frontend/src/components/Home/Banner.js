d7e89e49fee8c1935843b966cfd192ec4609322a
nivrichter <nivrichter@gmail.com>
2022-08-30 11:43:11 +0300

    yalla

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -2,12 +2,29 @@ import React from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = () => {
+  const handleSearch = (title) => {
+    if (title.length < 3) {
+      title = "";
+    }
+    props.onSearch(
+      title,
+      (page) => agent.Items.byTitle(title, page),
+      agent.Items.byTitle(title)
+    );
+  };
+  
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input
+            type="text"
+            id="search-box"
+            onChange={(e) => handleSearch(e.target.value)}
+            placeholder="What is it you truly desire?"
+          />
           <span> the cool stuff.</span>
         </div>
       </div>
