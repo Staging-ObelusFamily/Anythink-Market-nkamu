b3b147dfefbb3e1592a97cbc5db8e59eb521107f
nivrichter <nivrichter@gmail.com>
2022-08-30 12:11:05 +0300

    asd

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = (props) => {
+  const [showSearch, setshowSearch] = useState(false);
   const handleSearch = (title) => {
     if (title.length < 3) {
       title = "";
     }
     props.onSearch(
-      title,
-      (page) => agent.Items.byTitle(title, page),
-      agent.Items.byTitle(title)
+        title,
+        (page) => agent.Items.byTitle(title, page),
+        agent.Items.byTitle(title)
     );
   };
 
   return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span id="get-part">A place to get</span>
-          <input
-            type="text"
-            id="search-box"
-            onChange={(e) => handleSearch(e.target.value)}
-            placeholder="What is it you truly desire?"
-          />
-          <span> the cool stuff.</span>
+      <div className="banner text-white">
+        <div className="container p-4 text-center">
+          <img src={logo} alt="banner" />
+          <div>
+          <span onClick={() => setshowSearch(true)} id="get-part">
+            A place to get
+          </span>
+            {showSearch ? (
+                <input
+                    type="text"
+                    id="search-box"
+                    onChange={(e) => handleSearch(e.target.value)}
+                    placeholder="What is it you truly desire?"
+                />
+            ) : (
+                ""
+            )}
+            <span> the cool stuff.</span>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
